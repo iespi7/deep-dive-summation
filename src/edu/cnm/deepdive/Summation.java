58929b90c4c49caf5c80f5bd2ced94fb0183f5ea
iespi7 <ivanespinoza262@gmail.com>
2018-05-21 13:17:57 -0600

    Initial commit.

---
+++ src/edu/cnm/deepdive/Summation.java
@@ -0,0 +1,18 @@
+package edu.cnm.deepdive;
+
+public class Summation {
+
+  public static void main(String[] args) {
+//    System.out.println(args[0]);
+//    System.out.println(args[1]);
+//    System.out.println(args.length);
+    int sum = 0;
+    for (int i = 0; i < args.length; i = i + 1) {
+      int val = Integer.parseInt(args[i]);
+      sum = sum + val;
+//      System.out.println(args[i]);
+    }
+    System.out.println(sum);
+  }
+
+}

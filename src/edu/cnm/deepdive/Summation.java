56ad81ae839d29f9c843d9d9cf730c6b4ff8c81d
iespi7 <ivanespinoza262@gmail.com>
2018-05-21 16:29:49 -0600

    Changed for loop to while loop; added first version of Javadoc

--- src/edu/cnm/deepdive/Summation.java
+++ src/edu/cnm/deepdive/Summation.java
@@ -1,13 +1,28 @@
 package edu.cnm.deepdive;
 
+/**
+ * This is the class containing the main entry point for the (eventual) RPN calculator.
+ * 
+ * @author ivane
+ * @version 0.1
+ */
 public class Summation {
 
+  /**
+   * Command-line entry point for RPN interpreter.
+   * 
+   * @param args operands and operators.
+   */
   public static void main(String[] args) {
 
     int sum = 0;
-    for (int i = 0; i < args.length; i = i + 1) {
+
+    int i = 0;
+    while (i < args.length) {
+      args[i] = args[i].trim();
       int val = Integer.parseInt(args[i]);
       sum = sum + val;
+      i++;
     }
     System.out.println(sum);
   }
